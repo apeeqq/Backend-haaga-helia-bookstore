30b97b7a27efeae5a781fd64f88e9b988a817f70
apeeqq <151618573+apeeqq@users.noreply.github.com>
2026-09-06 14:38:38 +0300

    First commit

---
+++ src/main/java/backend/bookstore/domain/Book.java
@@ -0,0 +1,73 @@
+package backend.bookstore.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private LocalDate publicationYear;
+    private String isbn;
+    private BigDecimal price;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, LocalDate publicationYear, String isbn, BigDecimal price) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(LocalDate publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn=" + isbn
+                + ", price=" + price + "]";
+    }
+
+    
+
+}
