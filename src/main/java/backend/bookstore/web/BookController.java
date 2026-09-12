0cfb0dca31bd947955eef0f7d31f9ae91688dce1
apeeqq <151618573+apeeqq@users.noreply.github.com>
2026-09-12 20:34:54 +0300

    Add delete/add book button in endpoint /booklist to add or delete books

--- src/main/java/backend/bookstore/web/BookController.java
+++ src/main/java/backend/bookstore/web/BookController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import backend.bookstore.domain.Book;
 import backend.bookstore.domain.BookRepository;
 
 @Controller 
@@ -29,6 +32,24 @@ public class BookController {
     public String getBooklist(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id, Model model) {
+        repository.deleteById(id);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/save")
+    public String save(Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
     }
 
 }
