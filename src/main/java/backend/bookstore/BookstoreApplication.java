070ef5b95bd492a0b0fafcb267e9c04b36480762
apeeqq <151618573+apeeqq@users.noreply.github.com>
2026-09-19 19:45:34 +0300

    Add feature of adding new categories

--- src/main/java/backend/bookstore/BookstoreApplication.java
+++ src/main/java/backend/bookstore/BookstoreApplication.java
@@ -28,11 +28,11 @@ public class BookstoreApplication {
 		return (args) -> {
 
 			log.info("save some sample categories");
-			Category Sports= new Category("Sports");
-			Category Lastenkirjat = new Category("Lastenkirjat");
+			Category sports= new Category("Sports");
+			Category childrensBooks = new Category("Children's Books");
 
-			crepository.save(Sports);
-			crepository.save(Lastenkirjat);
+			crepository.save(sports);
+			crepository.save(childrensBooks);
 
 			log.info("save some sample books");
 			Book b1 = new Book("Aarteenmetsästys", "John Doe", 2000, "978-3-16-148410-0", new BigDecimal("15"));
@@ -43,8 +43,8 @@ public class BookstoreApplication {
 			brepository.save(b2);
 
 			log.info("Fetch all the categories");
-			log.info("Category: id={} Name={}", Sports.getCategoryid(), Sports.getName());
-			log.info("Category: id={} Name={}", Lastenkirjat.getCategoryid(), Lastenkirjat.getName());
+			log.info("Category: id={} Name={}", sports.getCategoryid(), sports.getName());
+			log.info("Category: id={} Name={}", childrensBooks.getCategoryid(), childrensBooks.getName());
 
 			log.info("Fetch all the books");
 			log.info("Book 1: id={} Title={}, Author={}, Year={}, isbn={}", b1.getId(), b1.getTitle(), b1.getAuthor(), b1.getPublicationYear(), b1.getIsbn());
--- src/main/java/backend/bookstore/web/CategoryController.java
+++ src/main/java/backend/bookstore/web/CategoryController.java
@@ -3,20 +3,22 @@ package backend.bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
 
+import backend.bookstore.domain.Category;
 import backend.bookstore.domain.CategoryRepository;
 
 @Controller
-public class categoryController {
+public class CategoryController {
 
     @Autowired 
     private CategoryRepository repository;
 
-        public categoryController() {
+        public CategoryController() {
     }
 
-    public categoryController(CategoryRepository repository) {
+    public CategoryController(CategoryRepository repository) {
         this.repository = repository;
     }
 
@@ -24,6 +26,18 @@ public class categoryController {
     public String getCategorylist(Model model) {
         model.addAttribute("categories", repository.findAll());
         return "categorylist";
+    }
+
+    @GetMapping("/addcategory")
+    public String addBook(Model model) {
+        model.addAttribute("category", new Category());
+        return "addcategory";
+    }
+
+    @PostMapping("/savecategory")
+    public String save(Category category) {
+        repository.save(category);
+        return "redirect:/categorylist";
     }
 
 }
