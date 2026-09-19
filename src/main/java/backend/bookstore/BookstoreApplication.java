0aa157fc884fc23daab7721ee4d06e52ed75a607
apeeqq <151618573+apeeqq@users.noreply.github.com>
2026-09-19 10:37:39 +0300

    Modify the logging of books to more logical structure

--- src/main/java/backend/bookstore/BookstoreApplication.java
+++ src/main/java/backend/bookstore/BookstoreApplication.java
@@ -24,14 +24,16 @@ public class BookstoreApplication {
 	@Bean
 	public CommandLineRunner books(BookRepository repository) {
 		return (args) -> {
-			log.info("save a couple of books");
+			log.info("save some sample books");
 			Book b1 = new Book("Aarteenmetsästys", "John Doe", 2000, "978-3-16-148410-0", new BigDecimal("15"));
-			log.info("Book 1 created: Title={}, Author={}, Year={}, isbn={}", b1.getTitle(), b1.getAuthor(), b1.getPublicationYear(), b1.getIsbn());
 			Book b2 = new Book("Hulivili", "Jane Doe", 1800, "072-3-16-135410-5", new BigDecimal("20"));
-			log.info("Book 2 created: Title={}, Author={}, Year={}, isbn={}", b2.getTitle(), b2.getAuthor(), b2.getPublicationYear(), b2.getIsbn());
 
 			repository.save(b1);
 			repository.save(b2);
+
+			log.info("Fetch all the books");
+			log.info("Book 1: id={} Title={}, Author={}, Year={}, isbn={}", b1.getId(), b1.getTitle(), b1.getAuthor(), b1.getPublicationYear(), b1.getIsbn());
+			log.info("Book 2: id={} Title={}, Author={}, Year={}, isbn={}", b2.getId(), b2.getTitle(), b2.getAuthor(), b2.getPublicationYear(), b2.getIsbn());
 		};
 	}
 
